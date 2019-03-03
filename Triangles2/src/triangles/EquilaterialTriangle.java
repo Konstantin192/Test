e3a4c38aff37fa50feb8ec66b7b86426b4add86a
Konstantin192 <konsstantin.georgiev@gmail.com>
2019-03-03 12:32:14 +0000

    Testing atom to netbeans update fetch

--- Triangles2/src/triangles/EquilaterialTriangle.java
+++ Triangles2/src/triangles/EquilaterialTriangle.java
@@ -3,20 +3,21 @@ package triangles;
 //blablabla test
 
 public class EquilaterialTriangle extends Triangle {
-    
+
     public EquilaterialTriangle() {
-        
+
     }
-    
+
     public EquilaterialTriangle(double side) {
         super(side);
     }
-    
+
     protected double area() {
         double area = a * a * Math.sqrt(3.0)/4.0;
+        System.out.println("Test");
         return area;
     }
-    
+
     public String displayName() {
         return "Equilateral Triangle(" + a + ")";
     }
